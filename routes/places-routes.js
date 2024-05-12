6180ab31929f9f20000a7aa58438ca0b348da153
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-05-12 19:58:25 +0100

    FEAT: Add error handling

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -34,6 +36,10 @@ router.get('/:pid', (req, res, next) => {
   const place = DUMMY_PLACES.find(p => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
   res.json({place}); // The same as { place: place }
 });
 
@@ -42,6 +48,11 @@ router.get('/user/:uid', (req, res, next) => {
   const places = DUMMY_PLACES.find(p => {
     return p.creator === userId;
   });
+  if (!places) {
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
+  }
   res.json({places});
 });
 
